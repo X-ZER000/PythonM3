c0a5b586492a247555738fb1fe3282fe893cdb9f
X-ZER000 <anabelleulate010@gmail.com>
2025-11-30 08:09:48 -0600

    Con Flask y web.

---
+++ Aprendiendo_rutas.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+facts_list = ["La mayoría de las personas que sufren adicción tecnológica experimentan un fuerte estrés cuando se encuentran fuera del área de cobertura de la red o no pueden utilizar sus dispositivos","Según un estudio realizado en 2018, más del 50% de las personas de entre 18 y 34 años se consideran dependientes de sus smartphones.","El estudio de la dependencia tecnológica es una de las áreas más relevantes de la investigación científica moderna","Según un estudio de 2019, más del 60% de las personas responden a mensajes de trabajo en sus smartphones en los 15 minutos siguientes a salir del trabajo","Una forma de combatir la dependencia tecnológica es buscar actividades que aporten placer y mejoren el estado de ánimo","Elon Musk afirma que las redes sociales están diseñadas para mantenernos dentro de la plataforma, para que pasemos el mayor tiempo posible viendo contenidos","Elon Musk también aboga por la regulación de las redes sociales y la protección de los datos personales de los usuarios. Afirma que las redes sociales recopilan una enorme cantidad de información sobre nosotros, que luego puede utilizarse para manipular nuestros pensamientos y comportamientos","Las redes sociales tienen aspectos positivos y negativos, y debemos ser conscientes de ambos cuando utilicemos estas plataformas"]
+
+datos_random = ["Los bebés nacen con aproximadamente 300 huesos, que se fusionan y reducen a 206 en la edad adulta","Venus es el único planeta que gira en sentido de las agujas del reloj.", "Cleopatra vivió más cerca de la invención del iPhone que de la construcción de las pirámides de Giza.", "Las jirafas tienen muchas más probabilidades de ser alcanzadas por un rayo que los humanos. "]
+
+@app.route("/")
+def home():
+    return """<h1>Esta es la página principal</h1>"
+    "<a href="/random_fact">¡Ver un dato aleatorio sobre el ambiente!</a>
+    "<a href="/random_datos">¡Ver un dato random de cualquier cosa!</a>"""
+
+
+@app.route("/dato")
+def random_fact():
+    return f'<p>{random.choice(facts_list)}</p>'
+
+@app.route("/random")
+def random_datos():
+    return f'<p>{random.choice(datos_random)}</p>'
+
+
+app.run(debug=True)
